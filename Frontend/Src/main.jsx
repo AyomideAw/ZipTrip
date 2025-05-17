61b4eb9217340652caba00db212dbb4eafa997af
Ayomide Awofisayo <ayomide_aw@Ayomides-MacBook-Pro-2.local>
2025-05-17 16:08:13 -0400

    Fix main.jsx entry and routing for Vercel

--- Frontend/Src/main.jsx
+++ Frontend/Src/main.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { AuthProvider } from "./context/AuthContext"; // <-- add this
+import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider> {/* <-- wrap App here */}
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
+
 
 
 // ReactDOM.createRoot(document.getElementById('root')).render(
